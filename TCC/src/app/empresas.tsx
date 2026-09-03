a17e37e796d3217cbee06862c6bdb40be4c3220d
Leonardo <wendellpassos16@gmail.com>
2026-09-02 22:04:17 -0300

    fazendo parte dos prototipo do tcc e começando a programar a parte da empresa

---
+++ TCC/src/app/empresas.tsx
@@ -0,0 +1,13 @@
+import react, { useState }  from "expo-router";
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Pressable,
+} from "react-native";
+
+export default function App() {
+    
+}
+
